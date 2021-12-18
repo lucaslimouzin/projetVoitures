79e1a6269b9455bebab6b1dfcc8325b8d8abdf24
lucaslimouzin <limouzin.lucas@gmail.com>
2021-12-18 19:54:32 +0100

    résolution de la réinitialisation du tableau et du problème sur la condition d'arret

--- projetVoitures/src/fr/lucas/classes/Execution.java
+++ projetVoitures/src/fr/lucas/classes/Execution.java
@@ -1,4 +1,5 @@
 package fr.lucas.classes;
+
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,66 +9,67 @@ public class Execution {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Operation m1 = new Operation();
-		ArrayList<Voiture> voitures = new ArrayList<Voiture>();		
-		//ArrayList<Voiture> voitures = new ArrayList<Voiture>();
+		ArrayList<Voiture> voitures = new ArrayList<Voiture>();
+		// ArrayList<Voiture> voitures = new ArrayList<Voiture>();
 		boolean quitter = false;
-		
+
 		System.out.println("Bonjour, vous allez enregistrer plusieurs voitures !");
-		
+
 		// demande à l'utilisateur son choix
 		System.out.println(" ");
 		System.out.println("***************** ");
 		System.out.println("Que voulez vous faire ?");
 		System.out.println("1) Ajouter un vehicule");
 		System.out.println("tapez le numéro de votre choix: ");
-		
+
 		System.out.println("***************** ");
 		System.out.println(" ");
 		Scanner clavier = new Scanner(System.in);
 		int reponse = clavier.nextInt();
-	
-		if(reponse == 1) {
-			voitures = m1.AjouterVoiture();
-							
+
+		if (reponse == 1) {
+			voitures = m1.AjouterVoiture(voitures);
+
 		}
-		
+
 		while (!quitter) {
 			// demande à l'utilisateur son choix
+			System.out.println(" ");
+			System.out.println("***************** ");
+			System.out.println("Que voulez vous faire ?");
+			System.out.println("1) Ajouter un vehicule");
+			System.out.println("2) Voir le tableau");
+			System.out.println("0) Quitter");
+			System.out.println("tapez le numéro de votre choix: ");
+			System.out.println("***************** ");
+			System.out.println(" ");
+			Scanner clavierChoix = new Scanner(System.in);
+			int choix = clavierChoix.nextInt();
+
+			if (choix == 1) {
+				m1.ajouterNouvelleVoiture = true;
+				voitures = m1.AjouterVoiture(voitures);
+				quitter = false;
+
+			} else if (choix == 2) {
 				System.out.println(" ");
 				System.out.println("***************** ");
-				System.out.println("Que voulez vous faire ?");
-				System.out.println("1) Ajouter un vehicule");
-				System.out.println("2) Voir le tableau");
-				System.out.println("0) Quitter");
-				System.out.println("tapez le numéro de votre choix: ");
-				System.out.println("***************** ");
 				System.out.println(" ");
-				Scanner clavierChoix = new Scanner(System.in);
-				int choix = clavierChoix.nextInt();
-			
-				if(choix == 1) {
-					voitures = m1.AjouterVoiture();
-					quitter = false;
-									
-				}else if (choix == 2)
-				{
-					System.out.println(" ");
-					System.out.println("***************** ");
-					System.out.println(" ");
-					
-					System.out.println("| Marque   | Couleur   | Porte   | Annee   |");
-					for (Voiture voiture: voitures) {
-						//afficher la voiture qui vient d'être créée						
-						voiture.AfficherTableau();
-	
+
+				System.out.println("| Marque   | Couleur   | Porte   | Annee   |");
+				for (Voiture voiture : voitures) {
+					// afficher la voiture qui vient d'être créée
+					voiture.AfficherTableau();
+
 				}
-					quitter = false;
-			} else if(choix ==0)
+				quitter = false;
+			} else if (choix == 0) {
 				System.out.println(" ");
 				System.out.println("***************** ");
 				System.out.println("Au revoir ");
 				System.out.println(" ");
 				quitter = true;
+			}
 		}
 	}
 }
--- projetVoitures/src/fr/lucas/classes/Operation.java
+++ projetVoitures/src/fr/lucas/classes/Operation.java
@@ -1,69 +1,64 @@
 package fr.lucas.classes;
+
 import java.util.Scanner;
 import java.util.ArrayList;
 
-public class Operation extends Voiture{
+public class Operation extends Voiture {
+	public boolean ajouterNouvelleVoiture = true;
 	
-	// methode pour ajouter une voiture 
-		public  ArrayList<Voiture> AjouterVoiture() {
-			ArrayList<Voiture> voitures = new ArrayList<Voiture>();		
-			boolean ajouterNouvelleVoiture = true;
-			boolean erreurSaisie = false;		
-			
-			while (ajouterNouvelleVoiture) {
-			
-				System.out.println(" ");
-				System.out.print("Ajout d'une nouvelle voiture ");
-				System.out.println(" ");
-				//pour entrer et enregistrer la marque de la voiture
-				System.out.print("Entrer la marque de la voiture : ");
-				Scanner clavierMarque = new Scanner(System.in);
-				String marque = clavierMarque.nextLine();
-				
-						
-				//pour entrer et enregistrer la couleur de la voiture
-				System.out.print("Entrer la couleur de la voiture : ");
-				Scanner clavierCouleur = new Scanner(System.in);
-				String couleur = clavierCouleur.nextLine();	
-				
-				//pour entrer et enregistrer l'année de la voiture
-				System.out.print("Entrer l'année de la voiture : ");
-				Scanner clavierAnnee = new Scanner(System.in);
-				int annee = clavierAnnee.nextInt();
-				
-				
-				//pour entrer et enregistrer le nb de portes de la voiture
-				System.out.print("Entrer le nombre de porte de la voiture : ");
-				Scanner clavierPorte = new Scanner(System.in);
-				int nb = clavierPorte.nextInt();
-				
-							
-				voitures.add (new Voiture(marque,couleur,annee,nb));
-				
-				// demande à l'utilisateur si il veut ajouter une nouvelle voiture
-				System.out.println(" ");
-				System.out.println("***************** ");
-				System.out.println("Voulez vous ajouter une autre voiture?");
-				System.out.println("1) Oui");
-				System.out.println("2) Non");
-				System.out.println("tapez le numéro de votre choix: ");
-				
-				System.out.println("***************** ");
-				System.out.println(" ");
-				Scanner clavierContinue = new Scanner(System.in);
-				int reponse = clavierContinue.nextInt();
-				
-			
-				if(reponse == 2) {
-					ajouterNouvelleVoiture = false;
-					break;					
-				}
+	// methode pour ajouter une voiture
+	public ArrayList<Voiture> AjouterVoiture(ArrayList<Voiture> newVoiture) {
+		
+
+		boolean erreurSaisie = false;
+
+		while (ajouterNouvelleVoiture) {
+
+			System.out.println(" ");
+			System.out.print("Ajout d'une nouvelle voiture ");
+			System.out.println(" ");
+			// pour entrer et enregistrer la marque de la voiture
+			System.out.print("Entrer la marque de la voiture : ");
+			Scanner clavierMarque = new Scanner(System.in);
+			String marque = clavierMarque.nextLine();
+
+			// pour entrer et enregistrer la couleur de la voiture
+			System.out.print("Entrer la couleur de la voiture : ");
+			Scanner clavierCouleur = new Scanner(System.in);
+			String couleur = clavierCouleur.nextLine();
+
+			// pour entrer et enregistrer l'année de la voiture
+			System.out.print("Entrer l'année de la voiture : ");
+			Scanner clavierAnnee = new Scanner(System.in);
+			int annee = clavierAnnee.nextInt();
+
+			// pour entrer et enregistrer le nb de portes de la voiture
+			System.out.print("Entrer le nombre de porte de la voiture : ");
+			Scanner clavierPorte = new Scanner(System.in);
+			int nb = clavierPorte.nextInt();
+
+			newVoiture.add(new Voiture(marque, couleur, annee, nb));
+
+			// demande à l'utilisateur si il veut ajouter une nouvelle voiture
+			System.out.println(" ");
+			System.out.println("***************** ");
+			System.out.println("Voulez vous ajouter une autre voiture?");
+			System.out.println("1) Oui");
+			System.out.println("2) Non");
+			System.out.println("tapez le numéro de votre choix: ");
+
+			System.out.println("***************** ");
+			System.out.println(" ");
+			Scanner clavierContinue = new Scanner(System.in);
+			int reponse = clavierContinue.nextInt();
+
+			if (reponse == 2) {
+				this.ajouterNouvelleVoiture = false;
+
 			}
-				
-				return voitures;
-				
-				
-				
-				
 		}
+
+		return newVoiture;
+
+	}
 }
